23471a06dfe2bd5bc7f204d82c1a061ac8160913
Om kumawat <126804270+Omkumawat-123@users.noreply.github.com>
2025-01-05 09:57:02 +0530

    Increment order in array element

---
+++ Array/RearrangeElement.java
@@ -0,0 +1,23 @@
+import java.util.*;;
+public class RearrangeElement {
+    public static void main (String arg[]){
+        int [] array={2,4,8,3,1,5,9,0,4};
+       
+
+        // increment
+        for (int i=0; i<array.length-1;i++){
+            for (int j=0;j<array.length-1-i;j++)
+            if (array[j]>array[j+1]) {
+                // swap
+                int temp=array[j];
+                array[j]=array[j+1];
+                array[j+1]=temp;
+                
+            }
+
+           
+        }
+        System.out.println(Arrays.toString(array));
+    }
+    
+}
