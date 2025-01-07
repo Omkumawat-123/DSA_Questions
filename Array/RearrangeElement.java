a5049a9e3aed25a8878a5e54a240adf073fd6d9b
Om kumawat <126804270+Omkumawat-123@users.noreply.github.com>
2025-01-07 10:39:12 +0530

    intial

--- Array/RearrangeElement.java
+++ Array/RearrangeElement.java
@@ -7,14 +7,14 @@ public class RearrangeElement {
         // increment
         for (int i=0; i<array.length-1;i++){
             for (int j=0;j<array.length-1-i;j++)
-            if (array[j]>array[j+1]) {
+            if (array[j]>array[j+1])  // current elment jar motha asla swap next element 
+            {        
                 // swap
                 int temp=array[j];
                 array[j]=array[j+1];
                 array[j+1]=temp;
                 
             }
-
            
         }
         System.out.println(Arrays.toString(array));
