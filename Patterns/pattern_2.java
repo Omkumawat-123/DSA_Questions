3fce0b07475f0da676e5805a41fff528f83922ec
Om kumawat <126804270+Omkumawat-123@users.noreply.github.com>
2025-01-08 10:48:03 +0530

    Pattern no 2

---
+++ Patterns/pattern_2.java
@@ -0,0 +1,18 @@
+
+//  * 
+//  * *
+//  * * *
+//  * * * *
+//  * * * * *
+
+public class pattern_2 {
+    public static void main (String arg []){
+        for (int i=0;i<5;i++){
+            for (int j=0;j<=i;j++){
+                System.out.print(" *");
+            }
+            System.out.println(" ");
+        }
+    }
+    
+}
