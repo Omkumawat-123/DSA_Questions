0b3eecdf97e70c3e4661546dc2b6151f2857d284
Om kumawat <126804270+Omkumawat-123@users.noreply.github.com>
2025-01-08 10:26:54 +0530

    Pattern 4*4

---
+++ Patterns/abc.java
@@ -0,0 +1,17 @@
+
+    // ****
+    // ****
+    // ****
+    // ****
+    
+public class abc {
+    public static void main(String Arg[]){
+        for(int i =0;i<4;i++){
+            for (int j=0;j<4;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+    
+}
