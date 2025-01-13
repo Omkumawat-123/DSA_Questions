4f430a68ac9c7588094c1af35e794721b7584d17
Om kumawat <126804270+Omkumawat-123@users.noreply.github.com>
2025-01-13 19:01:55 +0530

    Pattern 8

--- Patterns/pattern7.java
+++ Patterns/pattern7.java
@@ -1,3 +1,9 @@
+//       *      
+//      ***     
+//     *****    
+//    *******   
+//   *********  
+//  *********** 
 public class pattern7 {
     
     public static void main(String[] args) {
---
+++ Patterns/pattern8.java
@@ -0,0 +1,26 @@
+
+//*************
+// ***********
+//  *********     
+//   *******      
+//    *****
+//     ***
+//      *
+public class pattern8 {
+    public static void main(String[] args) {
+        for(int i=0;i<9;i++){
+            for(int j=0; j<i;j++){
+                System.out.print(" ");
+            }
+            for( int j = 0; j<2*9-(2*i+1);j++){
+                System.out.print("*");
+            }
+            for(int j=0; j<i;j++){
+                System.out.print(" ");
+            }
+            System.out.println(" ");
+        }
+    }
+   
+    
+}
