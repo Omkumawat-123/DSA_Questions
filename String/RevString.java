e3008504d14c800c5f5c343a0b6bff2280de3b74
Om kumawat <126804270+Omkumawat-123@users.noreply.github.com>
2025-01-13 19:32:47 +0530

    Rev String

---
+++ String/RevString.java
@@ -0,0 +1,16 @@
+
+
+public class RevString {
+    public static void main(String[] args) {
+        String str="abc";
+        char ch=0;
+        String RevString=" ";
+        for(int i=0;i<str.length();i++){
+            ch=str.charAt(i);
+            RevString=ch+RevString;
+            
+        }
+        System.out.println("Reversre String is:"+ RevString);
+    }
+    
+}
