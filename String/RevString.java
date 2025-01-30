f68703ab6d84d6237feef61e71e70cb1d69c782d
Om kumawat <126804270+Omkumawat-123@users.noreply.github.com>
2025-01-30 21:45:07 +0530

    rev string using diff method

--- String/RevString.java
+++ String/RevString.java
@@ -3,12 +3,16 @@
 public class RevString {
     public static void main(String[] args) {
         String str="abc";
-        char ch=0;
+        //char ch=0;
         String RevString=" ";
-        for(int i=0;i<str.length();i++){
-            ch=str.charAt(i);
-            RevString=ch+RevString;          // prpend 
+        // for(int i=0;i<str.length();i++){
+        //     ch=str.charAt(i);
+        //     RevString=ch+RevString;                      // usign prpend 
             
+        // }
+
+        for(int i=str.length()-1; i>=0 ; i--){
+            RevString=RevString+str.charAt(i);
         }
         System.out.println("Reversre String is:"+ RevString);
     }
